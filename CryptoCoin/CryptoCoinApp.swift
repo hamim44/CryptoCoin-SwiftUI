5896ec4ad8fba47b5f8008e163151c63a62ada06
Abrar hamim <abrarhamim4044@gmail.com>
2024-08-24 11:12:48 +0600

    Initial Commit

---
+++ CryptoCoin/CryptoCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinApp.swift
+//  CryptoCoin
+//
+//  Created by Abrar Hamim on 24/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
